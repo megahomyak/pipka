26c900d684b2b009d2fe74e4e2649b1791cdf0ea
megahomyak <g.megahomyak@gmail.com>
2026-06-11 21:40:51 +0500

    Now this is a good tool

--- pipka.py
+++ pipka.py
@@ -1,60 +1,35 @@
 import sys, venv, subprocess, os, pathlib, itertools
 
-def get_command_and_args():
-    return sys.argv[1], sys.argv[2:]
+# Getting args to proxy later:
+args = sys.argv[1:]
 
-def find_venv():
-    curdir = pathlib.Path.cwd()
-    for parent in itertools.chain([curdir], curdir.parents):
-        venv_path = (parent / ".venv")
-        if venv_path.is_dir():
-            return venv_path
-    raise FileNotFoundError("venv dir not found")
+# Finding or creating venv:
+curdir = pathlib.Path.cwd()
+for dir_ in itertools.chain([curdir], curdir.parents):
+    venv_path = (dir_ / ".venv")
+    if venv_path.is_dir():
+        break
+else:
+    venv.create(".venv", symlinks=True, with_pip=True)
+    venv_path = curdir / ".venv"
 
-def find_or_create_venv():
-    try:
-        return find_venv()
-    except FileNotFoundError:
-        venv.create(".venv", symlinks=True, with_pip=True)
-        return ".venv"
+# Creating env vars for in-venv executions:
+venv_env = os.environ.copy()
+if sys.platform == "win32":
+    executables_path = venv_path / "Scripts"
+else:
+    executables_path = venv_path / "bin"
+venv_env["PATH"] = f"{executables_path}{os.pathsep}{venv_env['PATH']}"
+venv_env["VIRTUAL_ENV"] = str(venv_path)
+venv_env.pop("PYTHONHOME", None) # idk if this helps
 
-def get_venv_env(venv_path):
-    venv_env = os.environ.copy()
-    if sys.platform == "win32":
-        executables_path = venv_path / "Scripts"
-    else:
-        executables_path = venv_path / "bin"
-    venv_env["PATH"] = f"{executables_path}{os.pathsep}{venv_env['PATH']}"
-    venv_env["VIRTUAL_ENV"] = str(venv_path)
-    venv_env.pop("PYTHONHOME", None) # idk if this helps
-    return venv_env
+# Running whatever there was the need to run *in* the venv:
+return_code = subprocess.run(args, env=venv_env).returncode
 
-def run_command_in_venv(venv_path, args):
-    return subprocess.run(args, env=get_venv_env(venv_path)).returncode
-
-def run_python_in_venv(venv_path, args):
-    return run_command_in_venv(venv_path, ["python", *args])
-
-def run_pip_in_venv(venv_path, args):
-    return_code = run_command_in_venv(venv_path, ["pip", *args])
+# If we were messing with pip, we need to freeze the outcome:
+if args[0] == "pip":
     with open(venv_path.parent / "requirements.txt", "wb") as f:
-        f.write(subprocess.check_output(["pip", "freeze"], env=get_venv_env(venv_path)))
-    return return_code
+        f.write(subprocess.check_output(["pip", "freeze"], env=venv_env))
 
-def main():
-    command, args = get_command_and_args()
-    if command in ("pip",):
-        venv_path = find_or_create_venv()
-        sys.exit(run_pip_in_venv(venv_path, args))
-    elif command in ("init",):
-        find_or_create_venv()
-    elif command in ("run",):
-        venv_path = find_or_create_venv()
-        sys.exit(run_command_in_venv(venv_path, args))
-    elif command in ("python", "py"):
-        venv_path = find_or_create_venv()
-        sys.exit(run_python_in_venv(venv_path, args))
-    else:
-        raise ValueError(f"command \"{command}\" unknown")
-
-main()
+# And finally, proxying the return code:
+sys.exit(return_code)
